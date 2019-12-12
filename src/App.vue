304d248b4cddfb751e77372de246ec76d875c784
chirsworking <chrisjorgework@protonmail.com>
2019-12-12 20:53:20 +0000

    setup the first version of the project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductList msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './components/ProductList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ProductList
   }
 }
 </script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li
+        v-for="product in products"
+        v-bind:key="product.name"
+      >{{product.title}} - {{product.price}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import shop from "@/api/shop";
+export default {
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    shop.getProducts(products => (this.products = products));
+  }
+};
+</script>
